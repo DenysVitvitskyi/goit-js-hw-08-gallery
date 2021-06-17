96bd39e2b92c8348fb77977f52f51be89539f060
DenysVitvitskyi <78040713+DenysVitvitskyi@users.noreply.github.com>
2021-06-18 00:52:55 +0300

    process

--- app.js
+++ app.js
@@ -63,3 +63,78 @@ const galleryItems = [
       description: 'Lighthouse Coast Sea',
     },
   ];
+
+const galleryContainer = document.querySelector('.js-gallery');
+const lightBox = document.querySelector('.js-lightbox')
+const lightBoxImage = document.querySelector('.lightbox__image');
+const btn = document.querySelector('[data-action="close-lightbox"]');
+const overlay = document.querySelector('.lightbox__overlay');
+
+
+
+
+const galleryMarkup = galleryItems;
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryContainer.addEventListener('click', onImageClick);
+btn.addEventListener('click', onModalClose);
+overlay.addEventListener('click', onModalClose);
+
+
+
+function makeGalleryMarkup(gallery) {
+    return gallery.map(({original, preview, description}) => {
+        return `
+        <li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="${original}"
+  >
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+        `
+    }).join('')
+};
+
+function onImageClick(event) {
+    event.preventDefault();
+    const image = event.target;
+    if (!image.classList.contains('gallery__image')) {
+        return;
+    };
+
+    lightBox.classList.add('is-open');
+    lightBoxImage.src = image.getAttribute("data-source");
+    lightBoxImage.alt = image.alt;
+
+    addEventListener('keyup', onClick);
+
+}
+
+function onModalClose() {
+    lightBox.classList.remove('is-open');
+    lightBoxImage.src = '';
+    lightBoxImage.alt = '';
+
+    removeEventListener('keyup', onClick)
+}
+
+function onClick(event) {
+
+    if (event.code === 'Escape') {
+        onModalClose();
+    }
+
+    if (event.code === 'ArrowRight') {
+
+      
+    }
+        
+    
+}
